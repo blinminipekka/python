cdcf482e20c1107f5c783c171324477bc1b57510
blinminipekka <halimiblin@gmail.com>
2025-11-25 16:08:56 +0100

    a

--- lesson7/typecasting.py
+++ lesson7/typecasting.py
@@ -20,15 +20,49 @@
 # result2=a+int(b)
 # print(type(b))
 # print(result2)
-/
 
-name=input("Enter your name:")
-print(f"Hellon{name}")
+#
+# name=input("Enter your name:")
+# print(f"Hellon{name}")
+#
+# age=input("Enter your age:")
+# print(type(age))
+#
+# num1=int(input("Enter the first number:"))
+# num2=int(input("Enter the second number:"))
+# result=num1+num2
+# print(result)
+#
+# try:
+#     result=10/2
+#     print(result)
+# except ZeroDivisionError:
+#     print("Oops! Tried to divide to zero")
+#
+# fruits={
+#     "apple":5
+#     "orange":7
+# }
 
-age=input("Enter your age:")
-print(type(age))
 
-num1=int(input("Enter the first number:"))
-num2=int(input("Enter the second number:"))
-result=num1+num2
-print(result)
+try:
+    print(fruits["Orange"])
+except KeyError:
+    print("The key does not exist")
+
+
+
+    try:
+        result=10/0
+        print(result)
+    except ZerodivisionError:
+        print("Invalid division by zero")
+    except TypeError:
+        print("Invalid type for vision")
+    except Exception as e:
+        print({e})
+
+divide_num(a/10,b/2)
+dividie_num(a:/,b/0)
+divide_num(a:/,b/'2')
+
