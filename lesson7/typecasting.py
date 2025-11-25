bbfea45f83d5293d0918cffefe6e920c51b43488
blinminipekka <halimiblin@gmail.com>
2025-11-25 15:44:25 +0100

    a

---
+++ lesson7/typecasting.py
@@ -0,0 +1,34 @@
+# age= 25
+# print(type(age))
+# age_as_str=str(age)
+# print(age_as_str,"of type",type(age_as_str))
+#
+# x=5
+# y=4.3
+#
+# result=x+y
+# print(type(result))
+#
+# age=25
+# message="I am "+ str(age) + " years old"
+# print(message)
+#
+# a=5
+# b="3"
+# print(type(b))
+# b1=int(b)
+# result2=a+int(b)
+# print(type(b))
+# print(result2)
+
+
+name=input("Enter your name:")
+print(f"Hellon{name}")
+
+age=input("Enter your age:")
+print(type(age))
+
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+result=num1+num2
+print(result)
