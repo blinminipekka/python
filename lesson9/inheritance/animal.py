8f2d030dae79b3cc49b45edd110344c249ac24b5
blinminipekka <halimiblin@gmail.com>
2025-12-02 16:09:09 +0100

    a

---
+++ lesson9/inheritance/animal.py
@@ -0,0 +1,22 @@
+class Animal:
+    def sound(self):
+        print("Some generic animal sound.")
+
+class Dog(Animal):
+    def sound(self):
+        print("Woof Woof")
+
+class Cat(Anima)
+    def sound(self):
+        print("Meow")
+
+
+
+animal = Animal()
+animal.sound()
+
+dog = Dog()
+dog.sound()
+
+cat = Cat()
+cat.sound()
