8122e7969f6c81e8a24742cc4d9148f6fbd63531
blinminipekka <halimiblin@gmail.com>
2025-12-09 15:28:03 +0100

    a

---
+++ lesson10/Encapsulation/index.py
@@ -0,0 +1,26 @@
+class Student:
+    def __init__(self,name,age):
+        self.__name=name
+        self.__age=age
+
+    def get_name(self):
+            return self.__name
+
+    def set_name(self,name):
+        self.__name=name
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self,age):
+        self.__age=age
+
+student1=Student("Daris",15)
+print("Name:",student1.get_name())
+
+student1.set_name("Blin")
+print("Name:",student1.get_name())
+
+print("Age:",student1.get_age())
+student1.set_age(16)
+print("Age:",student1.get_age())
