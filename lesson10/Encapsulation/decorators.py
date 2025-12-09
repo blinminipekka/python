66590856b0eda88d42af93c952777a43dc3ace5f
blinminipekka <halimiblin@gmail.com>
2025-12-09 16:03:17 +0100

    a

---
+++ lesson10/Encapsulation/decorators.py
@@ -0,0 +1,27 @@
+class Student:
+    def __init__(self,name,age):
+        self.__name=name
+        self.__age=age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name (self,name):
+        self.__name=name
+
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self,age):
+        self.__age=age
+
+student1=Student("Daris",15)
+
+print("Name:",student1.name)
+student1.name="Blin"
+print("Name:",student1.name)
